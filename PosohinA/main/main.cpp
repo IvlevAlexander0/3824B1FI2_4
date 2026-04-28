d0e5bcdacd369637fca9f51449078315dd86d845
Sasha <azeh342d@gmail.com>
2026-04-28 14:24:18 +0300

    delelted double realis

--- PosohinA/main/main.cpp
+++
@@ -1,9 +0,0 @@
-#include"../lib_interface/interface.h"
-
-
-int main() {
-	Interface menu;
-	std::cout << "Welcome to the polynomial calculator" << std::endl;
-	menu.input_polyn();
-	menu.actions();
-}
